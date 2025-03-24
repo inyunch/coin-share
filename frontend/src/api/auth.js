e892191c789adb19aee5ec0078b0b2b9fa97cfa6
OWO <owo@gmail.com>
2025-03-24 21:34:11 +0800

    - frontent

--- frontend/src/api/auth.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-import qs from 'qs';
-
-
-export const apiLogin = form => axios.post('/auth/login', qs.stringify(form) , { headers: { 'content-type': 'application/x-www-form-urlencoded' } }  );
-export const apiRefresh = () => axios.post('/auth/refresh');
-export const apiLogout = () => axios.post('/auth/logout');
--- frontend/src/api/user.js
+++
@@ -1,5 +0,0 @@
-import request from './req';
-import axios from 'axios';
-
-export const apiRegister = data => axios.post('/users', data);
-export const apiGetUserList = () => axios.get('/users');
